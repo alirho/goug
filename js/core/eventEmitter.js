15a6a61d255fef77117aa5e24ce6fc8c957d047c
alirho <alireza.hokmabadi@protonmail.com>
2025-11-12 15:24:25 +0330

    add try catch

--- js/core/eventEmitter.js
+++ js/core/eventEmitter.js
@@ -20,12 +20,19 @@ class EventEmitter {
 
     /**
      * Emits an event, calling all subscribed listeners with the provided data.
+     * Errors in one listener will not prevent others from being called.
      * @param {string} eventName - The name of the event to emit.
      * @param {*} data - The data to pass to the listeners.
      */
     emit(eventName, data) {
         if (this.events[eventName]) {
-            this.events[eventName].forEach(listener => listener(data));
+            this.events[eventName].forEach(listener => {
+                try {
+                    listener(data);
+                } catch (error) {
+                    console.error(`Error in listener for event "${eventName}":`, error);
+                }
+            });
         }
     }
 }
